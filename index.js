77dfde9c5b6a42a5d92c8c978a9015e8ca0b41a5
sebastian-meier <contact@sebastianmeier.eu>
2017-08-22 18:13:00 +0200

    basic functionality

--- index.js
+++ index.js
@@ -6,19 +6,55 @@
 
 'use strict';
 
+var fs = require('fs'),
+  ar = require('archiver'),
+  utils = require('svift-utils')
+
 var bundle = (function () {
  
   var module = {};
 
   /**
-  * Initiate the rendering process by sending a data object containing params, vis and data (see data/example.json for structure)
+  * ZIP a folder, name it like the folder and move it into the folder after its done, then invoke the callback
   *
-  * @param {Object} `data` Parameters for the rendering process.
+  * @param {String} `folder` Path to the folder that should be zipped
+  * @param {Function} `callback` Callback function when its done
+  * @param {Boolean} `delete` true > zipped folder is deleted after zipping, false > move zip into the folder
   * @api public
   */
 
-  module.bundle = function (callback) {
-    callback();
+  module.bundle = function (folder, callback, delete) {
+    var split = folder.split('/'),
+      name = split[split.length-1],
+      path = folder.substr(0,folder.length-name.length),
+      output = fs.createWriteStream(path + name + '.zip')
+
+      var archive = ar('zip', {
+          zlib: { level: 9 }
+      })
+
+      output.on('close', function() {
+        if(delete){
+          utils.deleteFolderRecursive(path + name)
+        }else{
+          fs.renameSync(path + name + '.zip', path + name + '/' + name + '.zip')
+        }
+        callback()
+      })
+
+      archive.on('warning', function(err) {
+        if (err.code === 'ENOENT') {
+            // log warning 
+        } else {
+            throw err
+        }
+      })
+       
+      archive.on('error', function(err) {
+        throw err
+      })
+
+      archive.pipe(output)
   };
 
   return module;
